11d56aabc04d1aafcdec32bea06f7f2dd70dad67
Yi Chao <yichao@Yis-MacBook-Pro.local>
2023-04-26 22:54:46 -0700

    save progress

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+      props: {
+        allPostsData,
+      },
+    };
+  }
+;
